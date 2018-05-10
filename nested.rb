3c802651306d5b67ff1cbdfd017968cac54a5d0d
Christia T <christiatidwell@bellsouth.net>
2018-05-10 14:24:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ programmer_hash[:yukihiro_matsumoto] =
      {:known_for => "Ruby",
 		     :languages => ["LISP", "C"]}
 
-return programmer_hash				 
+return programmer_hash
 
 end
 
@@ -106,7 +106,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
